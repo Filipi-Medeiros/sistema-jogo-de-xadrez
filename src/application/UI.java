7a420e11eab90bbbf1cf7a5fa3ccb969e054008b
Filipi Medeiros <filipimedeiros112@gmail.com>
2024-09-18 16:00:39 -0300

    Bishop

--- src/application/UI.java
+++ src/application/UI.java
@@ -112,11 +112,11 @@ public class UI {
 		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
 		System.out.println("Captured pieces:");
-		System.out.print("White ");
+		System.out.print("White -> ");
 		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
 		System.out.print(ANSI_RESET);
-		System.out.print("Black ");
+		System.out.print("Black -> ");
 		System.out.print(ANSI_RED);
 		System.out.println(Arrays.toString(black.toArray()));
 		System.out.print(ANSI_RESET);
